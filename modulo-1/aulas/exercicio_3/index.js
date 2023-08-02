464ed7d2587a9233a6868a4d356ced5b9bd3ce77
ThiagoAguirre <thiago.aguirre08@outlook.com>
2023-08-02 17:39:46 -0300

    novos exercicios

--- modulo-1/aulas/exercicio_3/index.js
+++ modulo-1/aulas/exercicio_3/index.js
@@ -1,2 +1,15 @@
 const nome = "Thiago";
 console.log("Thiago");
+
+const primeiroNumero = 1;
+const segudnoNumero = 1;
+const resultado = primeiroNumero + segudnoNumero;
+const resultadoDois = resultado * 2;
+let resuladoTres = resultado * 10;
+resuladoTres = resuladoTres * 12;
+
+console.log(resultado);
+console.log(resultadoDois);
+console.log(resuladoTres);
+console.log(typeof primeiroNumero);
+console.log(typeof nome);
---
+++ modulo-1/desafio/desafio_1/index.js
@@ -0,0 +1,9 @@
+let nome = "Thiago";
+nome = "Thiago de Oliveira";
+const sobrenome = "Aguirre";
+const idade = 21;
+const peso = 109;
+const alturaEmCm = 1.8;
+
+console.log("meu nome é",nome,sobrenome,"tenho",idade,"anos, peso ",peso,"e tenho",alturaEmCm,"cm"
+);
