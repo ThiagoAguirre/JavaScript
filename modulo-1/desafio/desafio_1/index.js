dea5d442adfb8d854ce7fc1137efc17b960208a6
ThiagoAguirre <thiago.aguirre08@outlook.com>
2023-08-02 17:49:43 -0300

    desafio do imc

--- modulo-1/desafio/desafio_1/index.js
+++ modulo-1/desafio/desafio_1/index.js
@@ -1,9 +1,20 @@
+/* Nome: Thiaog de Oliveira Aguirre
+   peso: 109kg
+   altura: 1,80cm
+   Data de nascimento
+*/ 
+
 let nome = "Thiago";
+
 nome = "Thiago de Oliveira";
+
 const sobrenome = "Aguirre";
-const idade = 21;
 const peso = 109;
 const alturaEmCm = 1.8;
+const AnodeNascimento = 2002
 
-console.log("meu nome é",nome,sobrenome,"tenho",idade,"anos, peso ",peso,"e tenho",alturaEmCm,"cm"
-);
+let IMC = peso / (alturaEmCm *alturaEmCm);
+let idade = 2023 - AnodeNascimento;
+
+console.log("meu nome é",nome,sobrenome,"tenho",idade,"anos,peso",peso);
+console.log("e tenho",alturaEmCm,"cm  e o seu Seu IMC é de :", IMC)
